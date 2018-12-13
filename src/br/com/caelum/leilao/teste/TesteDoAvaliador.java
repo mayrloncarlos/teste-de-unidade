703a5915f032904ad0eaa9002fd05d27c7806f02
mayrloncarlos <mayrloncarlos@gmail.com>
2018-12-13 07:24:54 -0300

    Criando um teste com JUnit e identificando um bug

--- src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -1,28 +1,38 @@
 package br.com.caelum.leilao.teste;
 
+import org.junit.Test;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
+import org.junit.Assert;
 
 public class TesteDoAvaliador {
 
-	public static void main(String[] args) {
+	@Test
+	public void main() {
+		// parte 1: cenario
 		Usuario joao = new Usuario("João");
 		Usuario maria = new Usuario("Maria");
 		Usuario jose = new Usuario("José");
 		
 		Leilao leilao = new Leilao("Playstation 4 novo");
 		
-		leilao.propoe(new Lance(joao, 300.0));
-		leilao.propoe(new Lance(maria, 400.0));
-		leilao.propoe(new Lance(jose, 250.0));
+		leilao.propoe(new Lance(joao, 250.0));
+		leilao.propoe(new Lance(maria, 300.0));
+		leilao.propoe(new Lance(jose, 400.0));
 		
+		// parte 2: acao
 		Avaliador leiloeiro = new Avaliador();
 		leiloeiro.avalia(leilao);
 		
-		System.out.println(leiloeiro.getMaiorLance());
-		System.out.println(leiloeiro.getMenorLance());
+		// parte 3: validacao
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
 
 	}
 
